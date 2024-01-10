f2835ca1a69e410db939fd2ed85afc082eae8c9b
Sun Kit Tsui <sunkittsui@gmail.com>
2024-01-10 10:32:45 -0600

    feat: Implement `From<&str>` for `GUID`

--- src/guid.rs
+++ src/guid.rs
@@ -36,7 +36,6 @@ impl TryFrom<&str> for GUID {
 
         for i in 0..16 {
             let mut byte = 0u8;
-
             let mut byte_index = 0;
             while byte_index < 2 {
                 if let Some(c) = chars.next() {

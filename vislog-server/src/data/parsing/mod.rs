ca35caac08ba2d3f4f3540ca0045a07f45081330
Sun Kit Tsui <sunkittsui@gmail.com>
2024-04-05 10:50:42 -0500

    feat(vislog-server): Implement in-memory caching for `ProgramsProvider`

--- vislog-server/src/data/parsing/mod.rs
+++ vislog-server/src/data/parsing/mod.rs
@@ -1,7 +1,7 @@
-use std::{fmt::Display, sync::Arc};
+use std::{collections::HashMap, fmt::Display, ptr::read, sync::Arc};
 
 use thiserror::Error;
-use tokio::sync::RwLock;
+use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 use vislog_core::{parsing::guid::GUID, Program};
 use vislog_parser::{parse_programs, ProgramParsingError};
 
@@ -44,22 +44,81 @@ pub mod json_providers;
 /// let cs_major: Program = serde_json::from_str(&(serde_json::to_string(&cs_major_json)?))?;
 /// dbg!(cs_major.title);
 /// ```
+
+struct ProviderCache {
+    programs: HashMap<GUID, Program>,
+    errors: Vec<ProgramParsingError>,
+}
+
 #[derive(Clone)]
-pub struct ProgramsProvider(Arc<RwLock<Box<dyn json_providers::JsonProvider>>>);
+pub struct ProgramsProvider {
+    json_provider: Arc<RwLock<Box<dyn json_providers::JsonProvider>>>,
+    cache: Arc<RwLock<ProviderCache>>,
+}
 
 impl ProgramsProvider {
     pub fn with(json_provider: Box<dyn json_providers::JsonProvider>) -> Self {
-        let provider = Arc::new(RwLock::new(json_provider));
-        Self(provider)
+        let json_provider = Arc::new(RwLock::new(json_provider));
+        let cache = ProviderCache {
+            programs: HashMap::new(),
+            errors: Vec::new(),
+        };
+        let cache = Arc::new(RwLock::new(cache));
+        Self {
+            json_provider,
+            cache,
+        }
     }
 
     pub async fn get_all_programs(&self) -> Result<(Vec<Program>, Vec<ProgramParsingError>)> {
-        let program_jsons = self.0.read().await.get_all_program_jsons()?;
-        Ok(parse_programs(program_jsons))
+        let cache = {
+            let read_cache_guard = self.cache.read().await;
+
+            if read_cache_guard.programs.is_empty() && read_cache_guard.errors.is_empty() {
+                drop(read_cache_guard);
+                let json_provider_read_guard = self.json_provider.read().await;
+                let write_cache_guard = self.cache.write().await;
+                Self::refresh_cache(json_provider_read_guard, write_cache_guard).await?;
+
+                // Reacquire read lock
+                self.cache.read().await
+            } else {
+                read_cache_guard
+            }
+        };
+
+        let mut programs: Vec<Program> = cache.programs.values().cloned().collect();
+        programs.sort();
+        let errors = cache.errors.to_vec();
+
+        Ok((programs, errors))
     }
 
-    pub async fn get_program(&self, _guid: GUID) -> Result<Program> {
+    pub async fn get_program(&self, _guid: GUID) -> Result<Option<Program>> {
         todo!()
+    }
+
+    /// SAFETY: There must not be a another read guard for `RwLockReadGuard<'a, ProviderCache>` in
+    /// the same execution "thread" to avoid deadlocks
+    async fn refresh_cache<'a>(
+        json_provider_read_guard: RwLockReadGuard<'a, Box<dyn json_providers::JsonProvider>>,
+        mut cache_write_guard: RwLockWriteGuard<'a, ProviderCache>,
+    ) -> Result<()> {
+        let program_jsons = json_provider_read_guard.get_all_program_jsons()?;
+        let (programs, errors) = parse_programs(program_jsons);
+
+        let programs = programs
+            .into_iter()
+            .map(|p| (p.guid.clone(), p))
+            .collect::<Vec<(GUID, Program)>>();
+
+        cache_write_guard.programs.clear();
+        cache_write_guard.errors.clear();
+
+        cache_write_guard.programs.extend(programs);
+        cache_write_guard.errors.extend(errors);
+
+        Ok(())
     }
 }
 

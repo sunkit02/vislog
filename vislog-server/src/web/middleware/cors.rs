31c31b69d5c9763dd5027932e8dd4ec0bf9915a1
Sun Kit Tsui <sunkittsui@gmail.com>
2024-04-07 15:13:26 -0500

    feat(vislog-server): Log cors header from middleware

--- vislog-server/src/web/middleware/cors.rs
+++ vislog-server/src/web/middleware/cors.rs
@@ -1,17 +1,27 @@
 use axum::{body::Body, http::Response};
+use tracing::{debug, instrument};
 
 use crate::CONFIGS;
 
+#[instrument(skip(res))]
 pub async fn mw_set_access_control_allow_origin(mut res: Response<Body>) -> Response<Body> {
+    let cors_header_key = "Access-Control-Allow-Origin";
+
     if let Some(cors) = &CONFIGS.cors {
         if cors.origins.len() >= 1 {
             res.headers_mut().insert(
-                "Access-Control-Allow-Origin",
+                cors_header_key,
                 cors.origins_to_string()
                     .parse()
                     .expect("Should be valid header value"),
             );
         }
+
+        debug!(
+            "Cors header: \"{}: {:?}\"",
+            cors_header_key,
+            res.headers().get("Access-Control-Allow-Origin")
+        );
     }
 
     res
